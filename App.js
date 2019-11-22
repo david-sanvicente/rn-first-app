c27d0b8ec6ac1189524275b1a07b55e00f9e46db
David-Sanvicente <sanvicented1@gmail.com>
2019-11-22 16:52:02 -0500

    testing major refactor

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working"
-  );
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("The text changed!")}
-      />
+    <View style={{ padding: 60 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
